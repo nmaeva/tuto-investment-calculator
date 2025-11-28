f35ff9bbf4355370279f657769a042bff766dff3
nmaeva <nmaeva@protonmail.com>
2025-11-28 13:47:12 +0100

    feat: add Result component

---
+++ src/components/Result.jsx
@@ -0,0 +1,21 @@
+export default function Result() {
+  return (
+    <table className='result'>
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interes (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      {/*
+        Fetch user Input & display results
+        TBODY
+        TR
+        TD
+       */}
+    </table>
+  );
+}
